713958201635ed78c0359f1312ebc76e88975797
shakilmahammud <hshakil987@gmail>
2020-12-27 23:27:24 +0600

    update

---
+++ JsOperator.js
@@ -0,0 +1,15 @@
+var a=5
+b=++a;
+
+//Arithmetic Operator ()
+
+var line1="Hello";
+var line2="World";
+
+console.log(line1 + line2);
+
+//Comparison Operator
+
+//Logical Operator
+
+//Conditional Operator
