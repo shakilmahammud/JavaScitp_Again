d4a50365c79cadd49114592c1ff82980f16276eb
shakilmahammud <hshakil987@gmail>
2020-12-30 00:42:49 +0600

    update

---
+++ DataType.js
@@ -0,0 +1,25 @@
+//string
+
+console.log("Hello \t World");
+
+console.log("Hello \n World");
+
+x="Programmer";
+//word print
+console.log(x[0])
+console.log(x[1])
+console.log(x[2])
+
+//slice
+
+z="name"
+z.slice(0,2)
+
+//substr() method
+z.substr(2)
+z.replace('br',"r");
+x="abc"
+x.concat(z)
+
+//Boolean
+true
